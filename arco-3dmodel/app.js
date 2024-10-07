6f16a66db38909372e22edcd3c5618e285781388
shinnosuke999 <144081711+shinnosuke999@users.noreply.github.com>
2024-10-07 20:02:07 +0900

    model回転

--- arco-3dmodel/app.js
+++ arco-3dmodel/app.js
@@ -75,7 +75,7 @@ function initThree() {
   loader.load('models/1human_and_dog.glb', function(gltf) {
     model = gltf.scene;
     model.scale.set(1, 1, 1);  // モデルのサイズを調整
-    model.rotation.x = Math.PI / 2;  // モデルを後ろに90度回転
+    model.rotation.z = Math.PI / 2;  // モデルを左に90度回転
     scene.add(model);
     model.visible = false;  // 初期状態では非表示
     console.log('3Dモデルが正常に読み込まれました');
