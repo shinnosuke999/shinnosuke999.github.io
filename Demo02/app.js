b6b5a4438ce499442ebf482b3e797c1b4fe6e1c6
shinnosuke999 <144081711+shinnosuke999@users.noreply.github.com>
2024-10-07 11:15:09 +0900

    demo6

--- Demo02/app.js
+++ Demo02/app.js
@@ -1,6 +1,5 @@
 // グローバル変数の宣言
-var camera, canvas, context, imageData, pixels, detector;
-var debugImage, warpImage, homographyImage;
+var camera, canvas, context, imageData, detector;
 
 // ページ読み込み完了時に実行される関数
 function onLoad(){
@@ -14,51 +13,24 @@ function onLoad(){
   camera.height = 240;
   
   // キャンバスのサイズを設定
-  canvas.width = parseInt(canvas.style.width);
-  canvas.height = parseInt(canvas.style.height);
-  
-  // ブラウザ互換性のためのチェックと設定
-  if (navigator.mediaDevices === undefined) {
-    navigator.mediaDevices = {};
-  }
-  
-  if (navigator.mediaDevices.getUserMedia === undefined) {
-    navigator.mediaDevices.getUserMedia = function(constraints) {
-      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-      
-      if (!getUserMedia) {
-        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
-      }
-
-      return new Promise(function(resolve, reject) {
-        getUserMedia.call(navigator, constraints, resolve, reject);
-      });
-    }
-  }
+  canvas.width = 960;
+  canvas.height = 720;
   
   // カメラへのアクセスを要求
   navigator.mediaDevices
     .getUserMedia({ video: true })
     .then(function(stream) {
-      if ("srcObject" in video) {
-        video.srcObject = stream;
-      } else {
-        video.src = window.URL.createObjectURL(stream);
-      }
+      camera.srcObject = stream;
+      camera.onloadedmetadata = function(e) {
+        camera.play();
+      };
     })
     .catch(function(err) {
       console.log(err.name + ": " + err.message);
-    }
-  );
+    });
     
-  // 画像処理用の変数を初期化
-  imageData = context.getImageData(0, 0, camera.width, camera.height);
-  pixels = [];
+  // 検出器を初期化
   detector = new AR.Detector();
-  
-  debugImage = context.createImageData(camera.width, camera.height);
-  warpImage = context.createImageData(49, 49);
-  homographyImage = new CV.Image();
   
   // アニメーションループを開始
   requestAnimationFrame(tick);
@@ -68,11 +40,10 @@ function onLoad(){
 function tick(){
   requestAnimationFrame(tick);
   
-  if (video.readyState === video.HAVE_ENOUGH_DATA){
+  if (camera.readyState === camera.HAVE_ENOUGH_DATA){
     snapshot();
 
     var markers = detector.detect(imageData);
-    drawDebug();
     drawCorners(markers);
     drawId(markers);
     displayMarkerInfo(markers);
@@ -81,82 +52,25 @@ function tick(){
 
 // ビデオフレームをキャンバスに描画
 function snapshot(){
-  context.drawImage(video, 0, 0, camera.width, camera.height);
-  imageData = context.getImageData(0, 0, camera.width, camera.height);
-}
-      
-// デバッグ情報を描画
-function drawDebug(){
-  var width = camera.width, height = camera.height;
-  
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  
-  context.putImageData(imageData, 0, 0);
-  context.putImageData( createImage(detector.grey, debugImage), width, 0);
-  context.putImageData( createImage(detector.thres, debugImage), width * 2, 0);
-  
-  drawContours(detector.contours, 0, height, width, height, function(hole) {return hole? "magenta": "blue";});
-  drawContours(detector.polys, width, height, width, height, function() {return "green";} );
-  drawContours(detector.candidates, width * 2, height, width, height, function() {return "red";} );
-  
-  drawWarps(detector.grey, detector.candidates, 0, height * 2 + 20);
-}
-
-// 輪郭を描画
-function drawContours(contours, x, y, width, height, fn){
-  var i = contours.length, j, contour, point;
-  
-  while(i --){
-    contour = contours[i];
-
-    context.strokeStyle = fn(contour.hole);
-    context.beginPath();
-
-    for (j = 0; j < contour.length; ++ j){
-      point = contour[j];
-      this.context.moveTo(x + point.x, y + point.y);
-      point = contour[(j + 1) % contour.length];
-      this.context.lineTo(x + point.x, y + point.y);
-    }
-    
-    context.stroke();
-    context.closePath();
-  }
-}
-
-// ワープ画像を描画
-function drawWarps(imageSrc, contours, x, y){
-  var i = contours.length, j, contour;
-  
-  var offset = ( canvas.width - ( (warpImage.width + 10) * contours.length) ) / 2
-  while(i --){
-    contour = contours[i];
-    
-    CV.warp(imageSrc, homographyImage, contour, warpImage.width);
-    this.context.putImageData( createImage(homographyImage, warpImage), offset + i * (warpImage.width + 10), y);
-    
-    CV.threshold(homographyImage, homographyImage, CV.otsu(homographyImage) );
-    this.context.putImageData( createImage(homographyImage, warpImage), offset + i * (warpImage.width + 10), y + 60);
-  }
+  context.drawImage(camera, 0, 0, canvas.width, canvas.height);
+  imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 }
 
 // マーカーの角を描画
 function drawCorners(markers){
-  var corners, corner, i, j;
-
   context.lineWidth = 3;
 
-  for (i = 0; i !== markers.length; ++ i){
-    corners = markers[i].corners;
+  for (var i = 0; i < markers.length; i++){
+    var corners = markers[i].corners;
     
     context.strokeStyle = "red";
     context.beginPath();
     
-    for (j = 0; j !== corners.length; ++ j){
-      corner = corners[j];
+    for (var j = 0; j < corners.length; j++){
+      var corner = corners[j];
+      var nextCorner = corners[(j + 1) % corners.length];
       context.moveTo(corner.x, corner.y);
-      corner = corners[(j + 1) % corners.length];
-      context.lineTo(corner.x, corner.y);
+      context.lineTo(nextCorner.x, nextCorner.y);
     }
 
     context.stroke();
@@ -167,41 +81,20 @@ function drawCorners(markers){
   }
 }
 
-// マーカーIDを描画←確認用
+// マーカーIDを描画
 function drawId(markers){
-  var corners, corner, x, y, i, j;
-  
   context.strokeStyle = "blue";
   context.lineWidth = 1;
+  context.font = "16px Arial";
   
-  for (i = 0; i !== markers.length; ++ i){
-    corners = markers[i].corners;
-    
-    x = Infinity;
-    y = Infinity;
-    
-    for (j = 0; j !== corners.length; ++ j){
-      corner = corners[j];
-      
-      x = Math.min(x, corner.x);
-      y = Math.min(y, corner.y);
-    }
+  for (var i = 0; i < markers.length; i++){
+    var corners = markers[i].corners;
+    var x = corners[0].x;
+    var y = corners[0].y;
 
-    context.strokeText(markers[i].id, x, y)
+    context.strokeText(markers[i].id, x, y - 15);
   }
 }
-
-// 画像データを作成
-function createImage(src, dst){
-  var i = src.data.length, j = (i * 4) + 3;
-  
-  while(i --){
-    dst.data[j -= 4] = 255;
-    dst.data[j - 1] = dst.data[j - 2] = dst.data[j - 3] = src.data[i];
-  }
-  
-  return dst;
-};
 
 // マーカー情報を表示
 function displayMarkerInfo(markers) {
@@ -210,12 +103,7 @@ function displayMarkerInfo(markers) {
   context.strokeStyle = "black";
   context.lineWidth = 3;
 
-  var text;
-  if (markers.length > 0) {
-    text = "マーカーID: " + markers[0].id;
-  } else {
-    text = "マーカー認識中";
-  }
+  var text = markers.length > 0 ? "マーカーID: " + markers[0].id : "マーカー認識中";
 
   var x = 10;
   var y = canvas.height - 10;
